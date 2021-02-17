5cecffd3d54847d9d2fa1b742c7f0179b7c8b2e4
Shenjiahang <1256752271@qq.com>
2021-02-17 22:30:24 +0800

    java文件

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.fanshe.lizi;
+
+public class Main {
+	public static void main(String[] args) {
+		String s="";
+		int[] ia=new int[3];
+		
+		String[] as={"afa","afasf"};
+		boolean[] ib= {true,false};
+		Class cls1=String.class;
+		
+		
+		System.out.println(s.getClass().toString());
+		System.out.println(ia.getClass().toString());
+		System.out.println(as.getClass().toString());
+		System.out.println(ib.getClass().toString());
+	}
+
+
+}
+
